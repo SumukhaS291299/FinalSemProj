e4a649ac6086ae6b6d13f5c32dd5d252a1f59476
TechNews-git <1by18is120@bmsit.in>
2022-04-28 17:53:15 +0530

    Updated Details.csv

--- ProduceIndividual.py
+++ ProduceIndividual.py
@@ -24,19 +24,3 @@ for i in currlisID:
     print(saveFIleName)
     NamesList.to_csv(saveFIleName)
 
-directory ="All_Details"
-
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        df = pd.read_csv(f)
-        listdf = df['Slno'].tolist()
-        num = len(listdf)
-        num = num-1
-        most_recent = df.loc[df['Slno'] == listdf[num]]
-        print(most_recent)
-        RecFileName = "Updated_Details"+ f
-        RecFileName = RecFileName.replace("All_Details","")
-        print("File placed at => " + RecFileName)
-        most_recent.to_csv(RecFileName)

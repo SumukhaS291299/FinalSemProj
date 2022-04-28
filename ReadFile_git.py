fd61f4c82960553bd04738efb51057dc692b56ac
TechNews-git <1by18is120@bmsit.in>
2022-04-28 14:03:59 +0530

    Changed Details.csv

--- ReadFile_git.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("https://raw.githubusercontent.com/SumukhaS291299/FinalSemProj/main/DronePPM.csv")
-
-print(df)
-
-print(df['Aux4'].tolist())
---
+++ Read_git.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/SumukhaS291299/FinalSemProj/master/Details.csv")
+
+print(df)
+
+# print(df['Aux4'].tolist())

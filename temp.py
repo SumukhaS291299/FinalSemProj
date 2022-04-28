1fd1cc25385795bd6e280017a3e44a45721ef1ec
TechNews-git <1by18is120@bmsit.in>
2022-04-28 17:11:06 +0530

    Updated Details.csv

--- temp.py
+++ temp.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/SumukhaS291299/FinalSemProj/master/Updated_Details/100.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/SumukhaS291299/FinalSemProj/master/Updated_Details/103.csv")
 
 print(df)

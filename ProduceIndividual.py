27e89786f3d7ce5b139772aa431f59f9aa166250
TechNews-git <1by18is120@bmsit.in>
2022-04-28 16:17:58 +0530

    All files update with new file

---
+++ ProduceIndividual.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# save a copy to a dataframe from github
+df = pd.read_csv("https://raw.githubusercontent.com/SumukhaS291299/FinalSemProj/master/Details.csv")
+
+# df.loc[df['column'] == row]  To get a perticular column with row
+
+# sorted = df.sort_values("patientSSID")      If required sort dataframe
+
+currlisID = []           # To create list of all patients
+AllID = df["patientSSID"].tolist()          # List of all ids
+
+for i in AllID:
+    if i not in currlisID:
+        currlisID.append(i)     #Append no dulpicate list of all Patients
+print(currlisID)
+
+for i in currlisID:
+    NamesList = df.loc[df['patientSSID'] == i]
+    print(NamesList)
+    saveFIleName = "All_Detais//" +str(i)
+    print(saveFIleName)
+    NamesList.to_csv(saveFIleName)
